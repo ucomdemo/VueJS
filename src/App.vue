5f3a40284238887fe2a69cf1e6e47e63827a8ff2
ucomdemo <ucomdemo111@gmail.com>
2025-09-24 11:51:34 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div style="text-align:center; margin-top:40px; font-size:2rem;">歡迎</div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
